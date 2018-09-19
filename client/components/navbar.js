5958d92318e3a2a2327ff7e15fa0f3477d86c266
veryspry <ehlinger.matt@gmail.com>
2018-09-19 16:10:00 -0500

    create responsive layout

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,34 +1,74 @@
 import React from 'react'
 
 
-const Navbar = () => {
-  return (
-    <nav id="large-navigation">
-      <ul>
-        <li className="nav-icon"><a href="/">Home</a></li>
-        <li className="nav-icon"><a href="/timeline">Blog</a></li>
-        {/* <li className="nav-icon"><a href="#music">Music</a></li> */}
-        {/* <li className="nav-icon"><a href="#development">Development</a></li> */}
-        {/* <li className="nav-icon"><a href="#contact">Contact</a></li> */}
+class Navbar extends React.Component {
 
-        <li className="social-icon">
-          <a href="https://github.com/veryspry" target="_blank" rel="noopener noreferrer" >
-          <i className="fa fa-github" /></a>
-        </li>
-        <li className="social-icon">
-          <a href="https://twitter.com/MattEhlinger" target="_blank" rel="noopener noreferrer" >
-          <i className="fa fa-twitter" /></a>
-        </li>
-        <li className="social-icon" >
-          <a href="https://www.instagram.com/veryspry/" target="_blank" rel="noopener noreferrer" >
-          <i className="fa fa-instagram" /></a>
-        </li>
+  state = {
+    width: window.innerWidth,
+  }
 
-        {/* <li className="nav-pic"><img src="/img/Headshots/square/IMG_9263_sq.jpg"/></li> */}
-      </ul>
-      <div className="nav-pic"><img src="/img/IMG_9263_sq.jpg"/></div>
-    </nav>
-  )
+  componentDidMount() {
+    window.addEventListener('resize', this.getWidth)
+  }
+
+  getWidth = () => {
+    this.setState({width: window.innerWidth})
+  }
+
+  render() {
+
+    if (this.state.width > 800) {
+      return (
+        <nav className="large-navigation">
+          <ul>
+            <li className="nav-icon"><a href="/">Home</a></li>
+            <li className="nav-icon"><a href="/timeline">Blog</a></li>
+            <li className="social-icon">
+              <a href="https://github.com/veryspry" target="_blank" rel="noopener noreferrer" >
+              <i className="fa fa-github" /></a>
+            </li>
+            <li className="social-icon">
+              <a href="https://twitter.com/MattEhlinger" target="_blank" rel="noopener noreferrer" >
+              <i className="fa fa-twitter" /></a>
+            </li>
+            <li className="social-icon" >
+              <a href="https://www.instagram.com/veryspry/" target="_blank" rel="noopener noreferrer" >
+              <i className="fa fa-instagram" /></a>
+            </li>
+
+            {/* <li className="nav-pic"><img src="/img/Headshots/square/IMG_9263_sq.jpg"/></li> */}
+          </ul>
+          <div className="nav-pic"><img src="/img/IMG_9263_sq.jpg"/></div>
+        </nav>
+      )
+    } else {
+      return (
+        <nav className="small-navigation">
+          <div className="small-nav-pic"><img src="/img/IMG_9263_sq.jpg"/></div>
+          <ul>
+            <li className="nav-icon"><a href="/">Home</a></li>
+            <li className="nav-icon"><a href="/timeline">Blog</a></li>
+          </ul>
+          <ul>
+            <li className="social-icon">
+              <a href="https://github.com/veryspry" target="_blank" rel="noopener noreferrer" >
+              <i className="fa fa-github" /></a>
+            </li>
+            <li className="social-icon">
+              <a href="https://twitter.com/MattEhlinger" target="_blank" rel="noopener noreferrer" >
+              <i className="fa fa-twitter" /></a>
+            </li>
+            <li className="social-icon" >
+              <a href="https://www.instagram.com/veryspry/" target="_blank" rel="noopener noreferrer" >
+              <i className="fa fa-instagram" /></a>
+            </li>
+
+            {/* <li className="nav-pic"><img src="/img/Headshots/square/IMG_9263_sq.jpg"/></li> */}
+          </ul>
+        </nav>
+      )
+    }
+  }
 }
 
 export default Navbar
