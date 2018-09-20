ab58c3e34479ccf0b1a3ac612e09ee07303ce422
veryspry <ehlinger.matt@gmail.com>
2018-09-20 12:05:07 -0500

    add footer and change landing page copy

---
+++ client/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>This site is built with Node, React and so much ❤️</h3>
+    </div>
+  )
+}
+
+export default Footer
