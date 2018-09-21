af5a69fb9ce537a8e00ca6c95debc0646c34d8ba
veryspry <ehlinger.matt@gmail.com>
2018-09-20 19:15:22 -0500

    remove AWS-blog

--- AWS-blog/src/AppSync.js
+++
@@ -1,6 +0,0 @@
-export default {
-    "graphqlEndpoint": "https://wt4kldeklzba3d2bbjqqsrq73u.appsync-api.us-east-2.amazonaws.com/graphql",
-    "region": "us-east-2",
-    "authenticationType": "API_KEY",
-    "apiKey": process.env.AWS_API_KEY
-}
